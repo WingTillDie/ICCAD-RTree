c64e32a1878b71b8f08729cbbd2af8eeaaedcbfd
WingTillDie <mychat.01a2bc3def@gmail.com>
2018-09-06 12:42:04 +0100

    Change all malloc to new and free to delete

--- DummyInsert.h
+++ DummyInsert.h
@@ -1,17 +1,10 @@
 #pragma once
 
-typedef struct critical_net
+struct critical_net
 {
 	int  id;
 	struct critical_net *next;
-} critical_net;
-double window_width;
-double min_width[20];
-double min_space[20];
-double max_fill_width[20];
-double min_density[20];
-double max_density[20];
-int total_layer;
-
+};
+//critical_net *critical_net_head;
 
 void rtree(char input_file_name[], char output_file_name[], critical_net *critical_net_head);
